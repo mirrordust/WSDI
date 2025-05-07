5a71b3e167dbdb70b012f5e171cf304a2a87ad97
mirrordust <gsmr19re@outlook.com>
2025-05-07 14:24:59 +0800

    页面滚动时header变小

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -2,10 +2,27 @@
 import IconUst from './icons/IconUst.vue'
 import IconLogo from './icons/IconLogo.vue'
 import IconMore from './icons/IconMore.vue'
+
+import { ref, onMounted, onUnmounted, computed } from 'vue'
+const scrollY = ref(0)
+const opacity = ref(0)
+
+const handleScroll = () => {
+  const currentY = window.scrollY || document.documentElement.scrollTop
+  scrollY.value = currentY
+  opacity.value = Math.min(currentY / 200, 1)
+}
+
+const hasScrolled = computed(() => {
+  return scrollY.value > 20 ? true : false
+})
+
+onMounted(() => window.addEventListener('scroll', handleScroll))
+onUnmounted(() => window.removeEventListener('scroll', handleScroll))
 </script>
 
 <template>
-  <header class="header">
+  <header class="header" :class="{ 'header-small': hasScrolled }">
     <div class="flex-item ust">
       <IconUst class="svg svg-ust" />
     </div>
@@ -67,12 +84,17 @@ import IconMore from './icons/IconMore.vue'
 
 @media (min-width: 1024px) {
   .header {
-    padding-left: 4%;
-    padding-top: 4%;
-    padding-bottom: 2%;
+    padding-left: 60px;
+    padding-top: 60px;
+    padding-bottom: 30px;
     display: flex;
     justify-content: flex-start;
     align-items: center;
+  }
+
+  .header-small {
+    padding-top: 20px;
+    padding-bottom: 20px;
   }
 
   .svg {
