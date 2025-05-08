e72d5a2bb3346b8bd190c3a8049ef3a038cab8ac
mirrordust <gsmr19re@outlook.com>
2025-05-08 17:06:39 +0800

    unesco news

---
+++ src/components/UnescoNews.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import UnescoNewsItem from './UnescoNewsItem.vue'
+</script>
+
+<template>
+  <div class="content-container">
+    <h2>News</h2>
+
+    <div class="all-news">
+      <UnescoNewsItem
+        image="/images/unesco_news/kickoff_meeting.png"
+        title="SEPRESS Kickoff Meeting, Online"
+        date=" April 21, 2025"
+      />
+    </div>
+
+    <div class="divider"></div>
+  </div>
+</template>
+
+<style scoped>
+.content-container {
+  padding-left: 90px;
+  padding-right: 90px;
+}
+
+h2 {
+  font-family: 'Poppins';
+  font-weight: 600;
+  font-size: 48px;
+  /* line-height: 100%; */
+  letter-spacing: 0%;
+  color: #0b0717;
+
+  padding-top: 30px;
+  padding-left: 30px;
+  padding-bottom: 60px;
+}
+
+.all-news {
+  padding-left: 30px;
+}
+
+.divider {
+  border-bottom: 1.5px solid #dddfdd;
+  width: 100%;
+  height: 0;
+  padding-top: 262px;
+}
+@media (min-width: 1024px) {
+}
+</style>
---
+++ src/components/UnescoNewsItem.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+defineProps({
+  image: {
+    type: String,
+    requird: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="news-item">
+    <div class="image">
+      <img :src="image" class="cover" />
+    </div>
+    <div class="title">{{ title }}</div>
+    <div class="date">{{ date }}</div>
+  </div>
+</template>
+
+<style scoped>
+.news-item {
+  /* text-decoration: none; */
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 270px;
+  /* height: 340px; */
+  border-top: 1.5px solid #dddfdd;
+}
+
+.image {
+  margin-top: 20px;
+  width: 100%;
+}
+
+.cover {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+
+.title {
+  font-family: 'Poppins';
+  font-weight: 700;
+  font-size: 20px;
+  /* line-height: 100%; */
+  letter-spacing: 0%;
+  color: #0b0717;
+
+  padding-top: 20px;
+}
+
+.date {
+  font-family: 'Poppins';
+  font-weight: 400;
+  font-size: 14px;
+  /* line-height: 100%; */
+  letter-spacing: 0%;
+  color: #7f7f7f;
+
+  padding-top: 20px;
+}
+</style>
